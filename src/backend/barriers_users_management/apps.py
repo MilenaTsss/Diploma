0b7b0df9feb133f696c110117a62de169b2b8eab
MilenaTsss <mvtsarakhova@edu.hse.ru>
2025-03-20 11:36:41 +0300

    added more apps

---
+++ src/backend/barriers_users_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BarriersUsersManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'barriers_users_management'
