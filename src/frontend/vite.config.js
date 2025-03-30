c8fae61cee5aa5dd326d23e579981afc260cb659
MilenaTsss <milena.czaraxova@mail.ru>
2025-03-30 22:24:17 +0300

    added new host settings

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    strictPort: true,
-  },
-});
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const allowedHosts = env.VITE_ALLOWED_HOSTS?.split(',') || []
+
+  return {
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
+      strictPort: true,
+      allowedHosts: allowedHosts,
+    },
+    preview: {
+      host: "0.0.0.0",
+      port: 4173,
+      strictPort: true,
+      allowedHosts: allowedHosts,
+    },
+  }
+})
