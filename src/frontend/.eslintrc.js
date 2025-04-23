288505ceefd82425c0099bf4a438a8b2e3ef1f75
DeadXDino <dskuchieva@edu.hse.ru>
2025-04-23 11:54:41 +0300

    fixed codestyle

--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -1,36 +1,36 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
-        project: './tsconfig.json',
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "jsx-a11y"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+  ],
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
     },
-    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y'],
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'airbnb',
-        'airbnb/hooks',
-        'airbnb-typescript',
-    ],
-    settings: {
-        react: { version: 'detect' },
-        'import/resolver': {
-            typescript: {
-                alwaysTryTypes: true,
-                project: './tsconfig.json',
-            },
-        },
-    },
-    rules: {
-        // Твои правила переопределения здесь
-        'react/react-in-jsx-scope': 'off', // если ты используешь React 17+
-        'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
-        '@typescript-eslint/no-unused-vars': ['warn'],
-    },
+  },
+  rules: {
+    // Твои правила переопределения здесь
+    "react/react-in-jsx-scope": "off", // если ты используешь React 17+
+    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+    "@typescript-eslint/no-unused-vars": ["warn"],
+  },
 };
